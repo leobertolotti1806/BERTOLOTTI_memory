fff54c0222c9d7f63bce2f5f2f56df2cc87f183d
leobertolotti1806 <120721601+leobertolotti1806@users.noreply.github.com>
2023-07-05 09:29:59 +0200

    Rimozione IA ultimaggio css

--- js/memory.js
+++ js/memory.js
@@ -1,5 +1,5 @@
 let table = document.getElementById("table");
-let h2 = document.querySelector("header > h2");
+let headerName = document.querySelectorAll("header > div > div");
 let pl = document.querySelectorAll(".pl");
 
 //vettore immagini
@@ -29,15 +29,9 @@ let canIclick = true;
 shuffleImgs();
 
 let formData = JSON.parse(localStorage.memory);
-let ia = {
-    card: [],
-    toCheck: [],
-    scoperte: []
-};
 
-pl[0].previousElementSibling.textContent = formData.pl1;
-pl[1].previousElementSibling.textContent = formData.pl2;
-h2.textContent = "Turno giocatore " + formData.pl1;
+headerName[0].textContent = formData.pl1;
+headerName[1].textContent = formData.pl2;
 
 
 for (let i = 0; i < 4; i++) {
@@ -49,6 +43,7 @@ for (let i = 0; i < 4; i++) {
         let col = document.createElement("div");
         col.className = "card";
         col.innerHTML = `<div><div></div></div>`;
+        //col.firstChild.innerHTML += imgs[arrInd];
         col.firstChild.firstChild.style.backgroundImage = "url(./img/" + imgs[arrInd] + ".png)";
 
         row.appendChild(col);
@@ -60,19 +55,9 @@ for (let i = 0; i < 4; i++) {
         });
 
         col.addEventListener("click", function () {
-            if (!cells[arrInd].trovata && this.firstChild.className == "" && canIclick) {
+            if (!cells[arrInd].trovata && col.firstChild.className == "" && canIclick) {
 
-                console.log(ia);
-
-                if (ia.scoperte.length == 14) {
-                    console.clear();
-                }
-
-                /* if (ia.scoperte.length == 15) {
-                    return;
-                } */
-
-                this.firstChild.className = "scopri";
+                col.firstChild.className = "scopri";
                 scoperte++;
 
                 if (scoperte % 2 == 1) {
@@ -80,9 +65,6 @@ for (let i = 0; i < 4; i++) {
                         id: cells[arrInd].id,
                         ind: arrInd,
                     };
-
-                    if (formData.ia)
-                        iaFunc(arrInd);
 
                 } else if (cells[arrInd].id == previous.id) {
                     //coppia
@@ -95,83 +77,40 @@ for (let i = 0; i < 4; i++) {
 
                     pl[turno].appendChild(card);
 
-
-                    if (formData.ia) {
-                        let iaInd = ia.card.findIndex(c => c.id == cells[arrInd].id);
-                        
-                        /* let posInd = ia.scoperte.findIndex(p => p == arrInd);
-                        ia.scoperte.splice(posInd, 0); */
-
-                        if (!ia.scoperte.includes(arrInd)) {
-                            ia.scoperte.push(arrInd);
-                        }
-
-                        if (!ia.scoperte.includes(previous.ind)) {
-                            ia.scoperte.push(previous.ind);
-                        }
-
-                        ia.card.splice(iaInd, 1);
-
-                        iaInd = ia.toCheck.findIndex(c => c.id == cells[arrInd].id);
-                        ia.toCheck.splice(iaInd, 1);
-
-                        if (turno == 1)
-                            iaFunc(arrInd);
-                    }
-
                     if (!cells.some(c => !c.trovata)) {
-                        alert("Vince il giocatore " + (turno + 1));
-                        if (confirm("Volete rigiocare?")) {
-                            shuffleImgs();
-
-                            previous = {};
-
-                            for (const k in cells) {
-                                cells[k].trovata = false;
-                                cells[k].card.firstChild.className = "";
-                                cells[k].card.firstChild.firstChild.style.backgroundImage = "url(./img/" + imgs[k] + ".png)";
-                                cells[k].id = imgs[k];
-                            }
-
-                            pl[0].innerHTML = "";
-                            pl[1].innerHTML = "";
-
-                            ia = {
-                                card: [],
-                                toCheck: [],
-                                scoperte: []
-                            };
-
-                            turno = 0;
-                            scoperte = 0;
-                        } else close();
+                        setTimeout(() => {
+                            alert("Vince il giocatore " + (turno + 1));
+                            if (confirm("Volete rigiocare?")) {
+                                shuffleImgs();
+    
+                                previous = {};
+    
+                                for (const k in cells) {
+                                    cells[k].trovata = false;
+                                    cells[k].card.firstChild.className = "";
+                                    cells[k].card.firstChild.firstChild.style.backgroundImage = "url(./img/" + imgs[k] + ".png)";
+                                    cells[k].id = imgs[k];
+                                }
+    
+                                pl[0].innerHTML = "";
+                                pl[1].innerHTML = "";
+    
+                                turno = 0;
+                                scoperte = 0;
+                            } else close(); 
+                        }, 500);
                     }
                 } else {
                     //cambio turno
-                    if (condition) {
-                        
-                    }
 
                     scoperte = 0;
-
                     canIclick = false;
                     turno ^= 1;
 
-                    h2.textContent = "Turno giocatore " + (turno == 0 ? formData.pl1 : formData.pl2);
-
-                    if (formData.ia) {
-                        let posInd = ia.scoperte.findIndex(s => s == arrInd);
-                        ia.scoperte.splice(posInd, 0);
-
-                        posInd = ia.scoperte.findIndex(s => s == previous.ind);
-                        ia.scoperte.splice(posInd, 0);
-
-                        iaFunc(arrInd);
-                    }
-
                     setTimeout(() => {
+                        headerName[0].parentElement.className = "turno" + (turno + 1);
                         canIclick = true;
-                        this.firstChild.className = "";
+                        col.firstChild.className = "";
                         cells[previous.ind].card.firstChild.className = "";
                     }, 800);
                 }
@@ -186,56 +125,5 @@ function shuffleImgs() {
     for (let i = imgs.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [imgs[i], imgs[j]] = [imgs[j], imgs[i]];
-    }
-}
-
-function iaFunc(arrInd) {
-
-    if (!ia.scoperte.includes(arrInd)) {
-        if (!ia.card.some(c => c.id == cells[arrInd].id)) {
-            /* prima volta scoperta */
-            ia.card.push({
-                id: cells[arrInd].id,
-                card: cells[arrInd].card,
-                ind: arrInd
-            });
-
-            ia.scoperte.push(arrInd);
-        } else {
-            /* già memorizzata */
-            ia.toCheck.push({
-                id: cells[arrInd].id,
-                card: cells[arrInd].card,
-                ind: arrInd
-            });
-        }
-    }
-
-    if (turno == 1) {
-        /* TOCCA a ia */
-        if (ia.toCheck.length > 0) {
-            if (scoperte % 2 == 1) {
-                setTimeout(() => {
-                    let IaCardId = ia.card.findIndex(c => c.id == ia.toCheck[0].id);
-                    ia.card[IaCardId].card.click();
-                }, 500);
-            } else {
-                setTimeout(() => {
-                    ia.toCheck[0].card.click();
-                    //se da errore metti cells[sfas]card
-                }, 1000);
-            }
-        } else {
-            let k;
-
-            do
-                k = Math.floor(Math.random() * 16);
-            while (cells[k].trovata || ia.scoperte.includes(k));
-
-            setTimeout(() => {
-                canIclick = true;
-                cells[k].card.click();
-            }, 850);
-        }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 let pl1 = document.getElementById("pl1");
 let pl2 = document.getElementById("pl2");
-let ia = document.getElementById("ia");
-let precPl2Value;
+let precPl2Value = "";
 
-ia.addEventListener("change", () => {
-    if (ia.checked) {
-        precPl2Value = pl2.value;
-        pl2.value = "I.A.";
-    } else pl2.value = precPl2Value;
+document.getElementById("table").onsubmit = (e) => {
+    if (pl1.value.length > 10) {
+        e.preventDefault();
+        alert("Giocatore 1, massimo caratteri 10");
 
-    pl2.readOnly = ia.checked;
-    pl2.classList.toggle("noFocus");
-});
+    } else if (pl2.value.length > 10) {
+        e.preventDefault();
+        alert("Giocatore 2, massimo caratteri 10");
 
-document.getElementById("table").onsubmit = () => {
-    localStorage.memory = JSON.stringify({
-        pl1: pl1.value,
-        pl2: pl2.value,
-        ia: ia.checked
-    });
+    } else if (pl1.value == pl2.value) {
+        e.preventDefault();
+        alert("Inserite nomi diversi!");
+
+    } else {
+        localStorage.memory = JSON.stringify({
+            pl1: pl1.value,
+            pl2: pl2.value,
+        });
+    }
 }
